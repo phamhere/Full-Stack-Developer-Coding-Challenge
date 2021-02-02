d8d31c40d29cf903a6ca585b270785d58a5d5e98
Peter Pham <phamhere@gmail.com>
2021-02-02 14:59:14 -0700

    altered db url to env variable for heroku, added temp code for reading json files

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
+import json
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/flask'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jdlgiettktpknc:f7a0ebfa34ce869bdb6ac7588ebd2e72a1cb48110132cbca914c4cf04996aa8c@ec2-54-235-103-219.compute-1.amazonaws.com:5432/d2dcs919qvqv22'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def home():
 	return "Hello World!"
+
+def seed_db():
+	with open('alerts.json') as f:
+		alerts = json.load(f)
+
+	with open('alerts.json') as f:
+		alerts = json.load(f)
+
+	pass
 
 if __name__ == '__main__':
 	from db import db
